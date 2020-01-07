5b8c8fcfba531ebaa4eb7d33fb07ff8520027eb9
Martin Kregar <martinkregar@gmail.com>
2020-01-07 19:38:49 +0100

    added dropdown with the options from localstorage

--- capstone/app.js
+++ capstone/app.js
@@ -18,4 +18,22 @@ submit.addEventListener('click', () => {
     // to retrive trip data
     //let trip = JSON.parse(localStorage.getItem(tripId))
 
+    const items = {...localStorage};
+
+    var section = document.getElementById('body')
+    var select = document.createElement("select");
+    let fragement = document.createDocumentFragment();
+    for (item in items) {
+        let trip = JSON.parse(localStorage.getItem(item))
+        var option = document.createElement("option");
+        option.text = `${trip.destination}, ${trip.date}`
+        option.value = item
+        select.add(option)
+        //select.options.add(new Option(trip.destination, trip.date))
+    }
+
+    fragement.appendChild(select)
+    section.appendChild(fragement)
+
+
 })
