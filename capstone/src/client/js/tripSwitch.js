58f9160a81482d9892a46993da4ee10b968d4570
Martin Kregar <martinkregar@gmail.com>
2020-01-25 15:14:19 +0100

    added date logic to tripSwitch

--- capstone/src/client/js/tripSwitch.js
+++ capstone/src/client/js/tripSwitch.js
@@ -2,17 +2,22 @@ function tripSwitch(id){
 
     // Switch global id to a previously saved trip
     window.id = id.value;
+    let today = Date.parse(new Date());
 
     // Find the old trip in localstorage
     const items = { ...localStorage };
     for (let item in items) {
         if (item === window.id) {
             let trip = JSON.parse(localStorage.getItem(item))
-            Client.getCurrentWeekForecast(trip.lat, trip.lng)
             Client.setTripText(trip.destination, trip.date)
             Client.setImages(trip.images)
             Client.setMapImage(trip.mapImage)
             Client.displayTodos();
+            if (trip.epochDate - today > 0 && trip.epochDate - today < 432000000) {
+                Client.getCurrentWeekForecast(trip.lat, trip.lng);
+              } else {
+                console.log('tbd');
+            };
         };
     };
 }
